44fa232dc1436a6e9a70b1854d7ba2413cda7623
Matt Snyder <snyder2112@me.com>
2013-08-13 09:22:07 -0400

    Initial implmentation of SNMPWalk in place

--- lib/fuey_client/fuey/inspections/snmp_walk.rb
+++ lib/fuey_client/fuey/inspections/snmp_walk.rb
@@ -1,6 +1,25 @@
+require "fuey_client/fuey/inspections/support/shell_command"
+
 module Fuey
   module Inspections
     class SNMPWalk
+
+      def initialize(title, ip, agent, oid, version='v1',community='public')
+        @title = title
+        @ip = ip
+        @oid = oid
+        @agent = agent
+        @version = version
+        @community = community
+      end
+
+      def execute
+        result = Support::ShellCommand.new("snmpwalk -#{@version} -c #{@community} #{@agent} #{@oid}").execute
+        result = result =~ /#{@ip}/
+        Log.write "[#{@title}] SNMPWalk for #{@ip} using #{@agent} #{result ? 'succeeded' : 'failed' }."
+        result
+      end
+
     end
   end
 end
---
+++ lib/fuey_client/fuey/inspections/support/shell_command.rb
@@ -0,0 +1,13 @@
+module Fuey
+  module Inspections
+    module Support
+      class ShellCommand
+        def initialize(command)
+        end
+
+        def execute
+        end
+      end
+    end
+  end
+end
