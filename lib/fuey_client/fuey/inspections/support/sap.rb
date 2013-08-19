4f4362c0947a5c6d44ae6178a00b9f668f054c4c
Matt Snyder <snyder2112@me.com>
2013-08-19 15:03:47 -0400

    Get SAPNWRFC working

--- lib/fuey_client/fuey/inspections/support/sap.rb
+++ lib/fuey_client/fuey/inspections/support/sap.rb
@@ -7,9 +7,9 @@ module Fuey
         end
 
         def ping
-          gem 'sapnwrfc'
-          SAPNW::Base.config =  @config
-          conn = SAPNW::Base.rfc_connect
+          require 'sapnwrfc'
+          ::SAPNW::Base.config =  @config
+          conn = ::SAPNW::Base.rfc_connect
           attrib = conn.connection_attributes
           fld = conn.discover("RFC_PING")
           fl = fld.new_function_call
@@ -19,7 +19,7 @@ module Fuey
           Log.write %(Could not ping SAP instance. The sapnwrfc gem is not installed)
           false
         rescue Exception => caught
-          Log.write %(RFC Ping for #{config['ashost']} failed due to #{caught})
+          Log.write %(RFC Ping for #{@config['ashost']} failed due to #{caught})
           return false
         ensure
           conn.close unless conn.nil?
--- lib/fuey_client/version.rb
+++ lib/fuey_client/version.rb
@@ -1,3 +1,3 @@
 module FueyClient
-  VERSION = "0.3.0"
+  VERSION = "0.3.3"
 end
